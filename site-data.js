2a73f8361bfc6137fc3890b88d440e66b3183437
JGWebStyle <2808joel@gmail.com>
2026-05-22 21:14:10 -0400

    Delete site-data.js

--- site-data.js
+++
@@ -1,5 +0,0 @@
-window.ANGELS_SITE_DATA = {
-  "dataUrl": "data/site-data.json",
-  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
-  "mode": "api"
-};
