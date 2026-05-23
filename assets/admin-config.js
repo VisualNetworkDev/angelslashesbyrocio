cc02a58015b3b9ab676eb35a9d710bdff74ee195
JGWebStyle <2808joel@gmail.com>
2026-05-22 21:14:23 -0400

    Add files via upload

---
+++ assets/admin-config.js
@@ -0,0 +1,4 @@
+window.ANGELS_ADMIN_CONFIG = {
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
+  "publicUrl": "https://visualnetworkdev.github.io/angelslashesbyrocio/"
+};
---
+++ assets/site-data.js
@@ -0,0 +1,5 @@
+window.ANGELS_SITE_DATA = {
+  "dataUrl": "data/site-data.json",
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
+  "mode": "api"
+};
