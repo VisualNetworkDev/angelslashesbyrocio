dcdb189eb0afeff362ba0cd2cde356e4b061794d
JGWebStyle <2808joel@gmail.com>
2026-05-22 21:02:08 -0400

    Update admin-config.js

--- admin-config.js
+++ admin-config.js
@@ -1,4 +1,4 @@
 window.ANGELS_ADMIN_CONFIG = {
-  "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec": "",
-  "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec": "./"
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
+  "publicUrl": "https://visualnetworkdev.github.io/angelslashesbyrocio/"
 };
