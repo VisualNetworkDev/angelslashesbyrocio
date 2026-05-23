78c1ca153fa2d2204dac49c08167a37bd2dd3b06
JGWebStyle <2808joel@gmail.com>
2026-05-22 21:01:44 -0400

    Update site-data.js

--- site-data.js
+++ site-data.js
@@ -1,5 +1,5 @@
 window.ANGELS_SITE_DATA = {
   "dataUrl": "data/site-data.json",
-  "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec": "",
-  "mode": "static"
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
+  "mode": "api"
 };
