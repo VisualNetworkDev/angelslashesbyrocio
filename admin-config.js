a96b5b148fa22f0646218962b410039b831dab2a
JGWebStyle <2808joel@gmail.com>
2026-05-22 21:14:03 -0400

    Delete admin-config.js

--- admin-config.js
+++
@@ -1,4 +0,0 @@
-window.ANGELS_ADMIN_CONFIG = {
-  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec",
-  "publicUrl": "https://visualnetworkdev.github.io/angelslashesbyrocio/"
-};
