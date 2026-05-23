e71958f8c6057618c51b5effe9e7293be1842c06
JGWebStyle <2808joel@gmail.com>
2026-05-22 20:49:28 -0400

    Update admin-config.js

--- admin-config.js
+++ admin-config.js
@@ -1,4 +1,4 @@
 window.ANGELS_ADMIN_CONFIG = {
-  "apiBaseUrl": "",
+  "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec": "",
   "https://script.google.com/macros/s/AKfycbwgK38IEFSorraDiy3p2o_xa32zMqC7NoWeHkraVunped0N4enjIKpfP1AVra461R6CQQ/exec": "./"
 };
